89c87f53d04242160c0613a33746fb2b58e8aa49
Isra Ashfaq <isra.ashfaq@tkxel.io>
2025-08-24 17:28:56 +0500

    chore: added comment to trigger CI pipeline

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+// This change will trigger the CI/CD pipeline - [Isra Ashfaq]
 const express = require('express');
 const app = express();
 const port = 3000;
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Hi there!');
+  res.send('Hi there, this my CI/CD Assignmentttt!');
 });
 
 // Another route example
 app.get('/about', (req, res) => {
-  res.send('This is my first Node.js web app for CI/CD pipeline');
+  res.send('This is my first Node.js web app for CI/CD pipelineee');
 });
 
 // Start the server
